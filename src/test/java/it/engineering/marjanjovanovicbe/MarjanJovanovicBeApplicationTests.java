d99fff76a439a290f0af82fb7f4a34abdb1b8a35
Marjan Jovanovic <marjan.jovanovic@eng.it>
2021-04-23 12:40:29 +0200

    Initial commit

---
+++ src/test/java/it/engineering/marjanjovanovicbe/MarjanJovanovicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.engineering.marjanjovanovicbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarjanJovanovicBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
