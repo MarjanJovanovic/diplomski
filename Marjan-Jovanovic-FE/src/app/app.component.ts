b0fb03bf94912e6af8057b34bbeb5b68f996f9ff
Marjan Jovanovic <marjan.jovanovic@eng.it>
2022-12-17 17:26:02 +0100

    bug fixes

--- Marjan-Jovanovic-FE/src/app/app.component.ts
+++ Marjan-Jovanovic-FE/src/app/app.component.ts
@@ -8,21 +8,24 @@ import { Roles } from './core/enums/roles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Marjan-Jovanovic-FE';
-  isLoggedIn: boolean;
+  isLoggedIn: boolean = true;
   isAdmin: boolean = false;
   isProfessor: boolean = false;
   isStudent: boolean = false;
-  userProfile: KeycloakProfile|null = null;
-  constructor(private keycloakService: KeycloakService, private router: Router) {
+  userProfile: KeycloakProfile | null = null;
+  constructor(
+    private keycloakService: KeycloakService,
+    private router: Router
+  ) {
     // keycloakService.isLoggedIn().then(res =>{
     //   this.isLoggedIn = res;
     // });
     // router.events.subscribe((val) => {
-    //   console.log("loggers") 
+    //   console.log("loggers")
     //   console.log("is professor: ", keycloakService.isUserInRole('ROLE_PROFESSOR'));
     //   console.log("is admin: ", keycloakService.isUserInRole('ROLE_ADMIN'));
     // });
@@ -32,22 +35,25 @@ export class AppComponent implements OnInit{
     this.isLoggedIn = await this.keycloakService.isLoggedIn();
 
     // type userRoles = Array<{id: number, text: string}>;
-    if (this.isLoggedIn){
+    if (this.isLoggedIn) {
       this.userProfile = await this.keycloakService.loadUserProfile();
       this.isAdmin = this.keycloakService.isUserInRole(Roles.ADMIN);
       this.isProfessor = this.keycloakService.isUserInRole(Roles.PROFESSOR);
       this.isStudent = this.keycloakService.isUserInRole(Roles.STUDENT);
     }
 
-    console.log("iis logged in???", this.isLoggedIn);
-    console.log("profile: ", this.userProfile);
-    console.log("is admin?: ", this.keycloakService.isUserInRole("ROLE_ADMIN"));
-    console.log("is professor?: ", this.keycloakService.isUserInRole("ROLE_PROFESSOR"));
-    console.log("get user roles", this.keycloakService.getUserRoles());
-    console.log("token", this.keycloakService.getToken());
+    console.log('iis logged in???', this.isLoggedIn);
+    console.log('profile: ', this.userProfile);
+    console.log('is admin?: ', this.keycloakService.isUserInRole('ROLE_ADMIN'));
+    console.log(
+      'is professor?: ',
+      this.keycloakService.isUserInRole('ROLE_PROFESSOR')
+    );
+    console.log('get user roles', this.keycloakService.getUserRoles());
+    console.log('token', this.keycloakService.getToken());
   }
 
-  login(){
+  login() {
     this.keycloakService.login();
   }
 
